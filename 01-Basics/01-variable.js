69fb261018feefd43b46bf47484ae2991000a016
SubhamReddddy <152278232+SubhamReddddy@users.noreply.github.com>
2023-11-28 12:22:23 +0000

    file added

---
+++ 01-Basics/01-variable.js
@@ -0,0 +1,17 @@
+/*
+1. variable is a container to store some data
+
+2.there are 3 types of varible in our js
+    1.let
+    2.const
+    3.var
+    var vs let
+        1. var
+        --------
+        we used var on before 2025 to assign some variable
+        functional scope 
+        2. let
+        --------
+        after es6 we use let to assign some variable
+        block scope
+*/
